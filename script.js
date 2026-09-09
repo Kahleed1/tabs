dad06ae9e65adb22cdf8819ff6469ab78a11e385
Kahleedaliyu <michaelaliyu1@gmail.com>
2026-09-09 08:54:04 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", initTabs);
+
+function initTabs() {
+    const tabs = document.querySelectorAll(".tab");
+
+    const contents = document.querySelectorAll('.tab-content');
+
+    tabs[0].classList.add("active");
+    contents[0].classList.add("active");
+
+
+    tabs.forEach(function(tab){
+        tab.addEventListener("click", function(){
+            const content = document.getElementById(this.dataset.tab);
+
+            tabs.forEach(tab => tab.classList.remove('active'));
+
+            contents.forEach(content => content.classList.remove('active'));
+
+            content.classList.add("active");
+            this.classList.add("active");
+        });
+    });
+
+
+
+
+};
